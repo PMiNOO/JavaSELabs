23885dcd5d1cc237cba2ff76a5c7cdf50c2dce3e
PMiNOO <alsgh9429@naver.com>
2025-08-11 17:35:14 +0900

    과제

--- Java_Practice/mylab/book/exception/PublicationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mylab.book.exception;
-
-public class PublicationNotFoundException extends Exception {
-    public PublicationNotFoundException(String message) {
-        super(message);
-    }
-}
